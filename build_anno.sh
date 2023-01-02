8226b23e9e49cd6f4e13a6a7c9bf5d0df1bf66a4
kwin <wonkung@163.com>
2023-01-02 20:08:07 +0800

    feat: build annotation

---
+++ build_anno.sh
@@ -0,0 +1,105 @@
+#!/bin/sh
+
+function command_line_option_include_item {
+    local list="$1"
+    local value="$2"
+    local result=1
+
+    for item in $list
+    do
+        if [ $item = $value ]; then
+            # found
+            echo "found --> $item : $value"
+            result=0
+        fi;
+    done
+    return $result
+}
+
+osname=`uname` # Linux/Darwin
+echo "osname: $osname"
+
+options="$@"
+echo "----------------0-----------------"
+echo $options
+command_line_option_include_item "$options" "pack"
+if [ $? -eq 0 ]; then
+    # make install package
+    pack_mode="debug"
+    command_line_option_include_item "$options" "release"
+    if [ $? -eq 0 ]; then
+        pack_mode="release"
+    fi
+    echo "----------------1-----------------"
+    echo "--> pack_mode：$pack_mode"
+    # sh ./pack.sh "$pack_mode"
+    exit 0
+fi
+
+command_line_option_include_item "$options" "install"
+if [ $? -eq 1 ]; then
+    echo "no install found, compile mode"
+    CMAKE_EXTRA_OPTIONS="-DCMAKE_BUILD_TYPE=Debug"
+    CBUILD_DIR="cbuild"
+    
+    source ./build_options.sh
+    
+    command_line_option_include_item "$options" "leak_trace"
+    if [ $? -eq 0 ]; then
+        CBUILD_DIR="${CBUILD_DIR}_leak"
+    fi
+
+    echo "CMAKE_EXTRA_OPTIONS: ${CMAKE_EXTRA_OPTIONS}"
+    echo "----------------2-----------------cd CBUILD_DIR"
+    echo "--> CBUILD_DIR: ${CBUILD_DIR}"
+    mkdir -p ${CBUILD_DIR}
+    cd ${CBUILD_DIR}
+    echo "----------------3-----------------printf options > ./build_options.inc"
+    echo "$options"
+    # printf "$options" > ./build_options.inc
+    
+    if [ $osname == "Linux" ]; then
+        # cmake3 .. ${CMAKE_EXTRA_OPTIONS}
+        echo "----------------4-----------------cmake3 .. CMAKE_EXTRA_OPTIONS"
+        echo ${CMAKE_EXTRA_OPTIONS}
+        CPU_CORE=$( lscpu -pCPU | grep -v "#" | wc -l )
+     
+    elif [ $osname == "Darwin" ]; then
+        # cmake .. ${CMAKE_EXTRA_OPTIONS}
+        echo "----------------5-----------------cmake3 .. CMAKE_EXTRA_OPTIONS"
+        echo ${CMAKE_EXTRA_OPTIONS}
+        CPU_CORE=$( sysctl hw|grep ncpu|awk -F ':' '{print $2}' )
+    fi
+    echo "----------------6-----------------CPU_CORE"
+    echo ${CPU_CORE}
+    
+    # # MEM_MEG=$( free -m | sed -n 2p | tr -s ' ' | cut -d\  -f2 )
+    
+    # # MEM_GIG=$(( ((MEM_MEG / 1000) / 2) ))
+    # # JOBS=$(( MEM_GIG > CPU_CORE ? CPU_CORE : MEM_GIG ))
+    # # make -j${JOBS}
+    if [ $CPU_CORE -le 4 ]; then
+        # make -j4
+        echo "83 line"
+    else
+        # make -j${CPU_CORE}
+         echo "84 line"
+    fi
+else
+    cbuild_path="cbuild"
+    command_line_option_include_item "$options" "release"
+    if [ $? -eq 0 ]; then
+        cbuild_path="cbuild_release"
+    fi
+
+    echo "install found, install mode from path:$cbuild_path"
+    echo "----------------7----------------- cd cbuild_path"
+    echo $cbuild_path
+    cd $cbuild_path
+  
+    echo "----------------8----------------- option"
+    echo $options
+    # printf "$options" > ./build_options.inc
+    # make install
+    # sudo ldconfig
+fi
